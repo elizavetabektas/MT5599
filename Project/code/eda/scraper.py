2a0963e44cc51cac04effffcdc66cfb8c20315eb
Yelizaveta Salo <ysalo1508@gmail.com>
2022-10-04 11:32:08 +0100

    Extracting location data from AR residents

--- Project/code/eda/scraper.py
+++ Project/code/eda/scraper.py
@@ -9,7 +9,7 @@ import pandas as pd
 import itertools
 
 now = datetime.now().replace(tzinfo=pytz.utc)
-start_date = (now - timedelta(days=90)).strftime('%Y-%m-%d')
+start_date = (now - timedelta(days=365)).strftime('%Y-%m-%d')
 end_date = (now + timedelta(days=1)).strftime('%Y-%m-%d')
 date_window = str(start_date) + "-" + str(end_date)
 path = "scaper_output.json"
@@ -17,20 +17,30 @@ scraper = sntwitter.TwitterSearchScraper
 
 
 def get_tweetlist(handle: str):
-        tweetlist = []
-        for idx, tweet in enumerate(scraper(f'from:{handle} since:{start_date} until:{end_date}').get_items()):
-            tweetlist.append({"id": str(tweet.id), "user": str(tweet.user.username), "DateTime": str(tweet.date), "tweet": str(tweet.content),
-            "user_location": str(tweet.user.location), "place": str(tweet.place), "coordinates": str(tweet.coordinates)})
-        print(f"{handle} completed!")
-        return tweetlist
+    tweetlist = []
+    for idx, tweet in enumerate(scraper(f'from:{handle} since:{start_date} until:{end_date}').get_items()):
+        tweetlist.append({"id": str(tweet.id), "user": str(tweet.user.username), "DateTime": str(tweet.date), "tweet": str(tweet.content),
+        "user_location": str(tweet.user.location),
+        "place_fullname": str(tweet.place.fullName),
+        "place_name": str(tweet.place.name),
+        "place_type": str(tweet.place.type),
+        "place_country": str(tweet.place.country),
+        "place_countrycode": str(tweet.place.countryCode),
+        "coordinates_longitude": str(tweet.coordinates.longitude),
+        "coordinates_latitude": str(tweet.latitude)})
+
+    print(f"{handle} completed!")
+    return tweetlist
 
 if __name__ == "__main__":
     with open('../data/argentina_users.txt') as f:
         handles = f.read().splitlines()
+    handles = handles[0:1000]
+    
     pool = Pool(processes=len(handles))
     results = []
     for result in tqdm.tqdm(pool.imap_unordered(get_tweetlist, handles), total=len(handles)):
         results.extend(result)
 
-    with open("../data/argentinian_users_90days.json", "w", encoding="utf-8") as f:
+    with open("../data/argentinian_users_90days_2.json", "w", encoding="utf-8") as f:
         json.dump(results, f, indent=4, ensure_ascii=False)
